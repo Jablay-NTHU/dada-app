6f2b441ad209d1e432484720c78b1a92b1447b02
bhimasta <agoengbhimasta@gmail.com>
2018-05-17 15:34:43 +0800

    fix logout error and invalid credential error

--- controllers/auth.rb
+++ controllers/auth.rb
@@ -5,7 +5,7 @@ require 'roda'
 module Dada
   # Web controller for Credence API
   class App < Roda
-    @login_route = '/auth/login'
+    @login_route = 'auth/login'
     route('auth') do |routing|
       routing.is 'login' do
         # GET /auth/login
@@ -17,21 +17,20 @@ module Dada
         routing.post do
           account = AuthenticateAccount.new(App.config).call(
             username: routing.params['username'],
-            password: routing.params['password'])
-
+            password: routing.params['password']
+          )
           session[:current_account] = account
           flash[:notice] = "Welcome back #{account['username']}!"
           routing.redirect '/'
         rescue StandardError
           flash[:error] = 'Username and password did not match our records'
-          routing.redirect @login_route
+          routing.redirect '/auth/login'
         end
       end
-
-      routing.on 'logout' do
+      routing.is 'logout' do
         routing.get do
           session[:current_account] = nil
-          routing.redirect @login_route
+          routing.redirect '/auth/login'
         end
       end
     end
