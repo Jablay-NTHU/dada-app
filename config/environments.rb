8b3d00809e53118ecd9cf2ff7c503aef09967b0c
林政文 <victorlin12345@linzhengwende-MacBook-Pro.local>
2018-05-24 18:11:53 +0800

    env change

--- config/environments.rb
+++ config/environments.rb
@@ -3,6 +3,7 @@
 require 'roda'
 require 'econfig'
 require 'rack/ssl-enforcer'
+require 'rack/session/redis'
 
 module Dada
   # Configuration for the API
@@ -14,14 +15,38 @@ module Dada
     Econfig.root = '.'
 
     configure :production do
-      use Rack::SslEnforcer
+      SecureSession.setup(config)
+      SecureMessage.setup(config)
+    end
+
+    ONE_MONTH = 30 * 24 * 60 * 60 # in seconds
+
+    configure :development, :test do
+
+      # use Rack::Session::Cookie,
+      # expire_after: ONE_MONTH, secret: config.SESSION_SECRET
+
+      use Rack::Session::Pool,
+      expire_after: ONE_MONTH
+
+      # use Rack::Session::Redis,
+      # expire_after: ONE_MONTH, redis_server: App.config.REDIS_URL
+
+    end
+
+    configure :production do
+      use Rack::SslEnforcer, :hsts => true
+
+      use Rack::Session::Redis,
+          expire_after: ONE_MONTH, redis_server: App.config.REDIS_URL
     end
 
     configure :development, :test do
       # Allows running reload! in pry to restart entire app
       def self.reload!
-        exec 'pry -r ./spec/test_load_all'
+        exec 'pry -r ./specs/test_load_all'
       end
     end
+    
   end
 end
