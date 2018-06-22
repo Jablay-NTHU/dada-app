55840329aefef28f699345c00763a9a850f57ff6
bhimasta <agoengbhimasta@gmail.com>
2018-06-22 14:26:09 +0800

    adding request detail modal

--- public/js/views/token/request.js
+++ public/js/views/token/request.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+  $('#response-table').on('click', 'tbody tr td a.show-response', function() {
+    alert('asu')
+    code = $(this).closest('tr').find('input.response-status').val();
+    header = $(this).closest('tr').find('input.response-header').val();
+    body = $(this).closest('tr').find('input.response-body').val();
+    // // Assign the value to modal
+    $('#status_code_input').attr('value', code);
+    document.getElementById('header_input').value = header;
+    document.getElementById('body_input').value = body;        
+  }); 
+
   $('#response-table').on('click', 'tbody tr td a.delete-response', function() {
     // Obtaining row information from token-table
     id = $(this).closest('tr').find('input.response-id').val();
